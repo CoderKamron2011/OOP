e75016bf69e790e946d8587d6509ca013c11403d
CoderKamron2011 <hasanovaruxshona774@gmail.com>
2024-03-02 09:10:02 +0200

    new codes

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿using OOP.service;
+Person person = new Person();
+System.Console.WriteLine(person.Last_name);
+System.Console.WriteLine(person.First_name);
+System.Console.WriteLine(person.Age);
--- service/newclass.cs
+++ service/newclass.cs
@@ -5,5 +5,27 @@ namespace OOP.service
         string first_name = "Kamron";
         string last_name = "mustafoyev";
         int age = 13;
+        public string First_name
+        {
+            get
+            {
+                return First_name;
+            }
+        }
+        public string Last_name
+        {
+            get
+            {
+                return Last_name;
+            }
+        }
+        public int Age
+        {
+           get
+           {
+                return Age;
+           } 
+        }
+        
     }
 }
